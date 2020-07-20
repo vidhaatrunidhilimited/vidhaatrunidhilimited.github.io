c900e57ffa9d4da7dd9b6ee9423825921597e089
nischith p <55499740+NISCHITH-p@users.noreply.github.com>
2020-07-20 19:13:55 +0530

    new one

--- scripts.js
+++
@@ -1,5 +0,0 @@
-var blink_speed = 500;
-var t = setInterval(function () {
-  var ele = document.getElementById('blinker');
-  ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
-}, blink_speed);
